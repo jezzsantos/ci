56ac23041030d8a607cc79a3ec480632186f00b4
Ilya Finkelshteyn <ilyaf@appveyor.com>
2017-11-06 22:25:02 -0800

    Update enable-rdp.ps1

--- scripts/enable-rdp.ps1
+++ scripts/enable-rdp.ps1
@@ -11,6 +11,7 @@ if($env:appveyor_rdp_password) {
     # change password
     $objUser = [ADSI]("WinNT://$($env:computername)/appveyor")
     write-host "Before: $?"
+    write-host "$password"
     $objUser.SetPassword($password)
     write-host "After: $?"
     [Microsoft.Win32.Registry]::SetValue("HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon", "DefaultPassword", $password)
